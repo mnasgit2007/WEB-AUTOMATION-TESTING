9741a30aebe1394ddee03a611a5b5069f0d7719b
Natsir <natsir07@gmail.com>
2026-09-07 22:25:49 +0800

    add products page object

---
+++ pages/products_page.py
@@ -0,0 +1,13 @@
+from pages.base_page import BasePage
+
+
+class ProductPage(BasePage):
+    
+    def __init__(self, page):
+        super().__init__(page)
+        
+        self.user_email = (page.get_by_test_id("user-email"))
+        
+        self.products_title = (page.get_by_test_id("product-title"))
+        
+        
