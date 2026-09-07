867e51d3dd8a70702f20f91c77fc309de3ffeec7
Natsir <natsir07@gmail.com>
2026-09-07 23:24:16 +0800

    test: configure pytest markers

--- tests/ui/auth/test_login.py
+++ tests/ui/auth/test_login.py
@@ -2,6 +2,11 @@ from playwright.sync_api import expect
 from config.settings import Settings
 from data.login_data import LoginExpected
 
+import pytest
+
+
+@pytest.mark.smoke
+@pytest.mark.regression
 def test_login_positive(
     login_page,
     products_page,
